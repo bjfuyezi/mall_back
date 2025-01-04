510d9cf16e22a4442bd8761b14f6dcf03d5aaa8d
Your Name <you@example.com>
2025-01-05 01:48:16 +0800

    购物车、优惠券模块部分测试+字段更改

--- src/main/java/com/example/demo/config/Utils.java
+++ src/main/java/com/example/demo/config/Utils.java
@@ -2,6 +2,9 @@ package com.example.demo.config;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.Locale;
 
@@ -9,6 +12,19 @@ public class Utils {
     public static Date TimetoDate(String s) throws ParseException {
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return simpleDateFormat.parse(s);
+    }
+
+    public static int TimeDiff(String dateString1,String dateString2) throws ParseException {
+        LocalDate date1 = LocalDate.parse(dateString1);
+        LocalDate date2 = LocalDate.parse(dateString2);
+        System.out.println(date1);
+        System.out.println(date2);
+        Duration duration = Duration.between(date1.atStartOfDay(), date2.atStartOfDay());
+        int days = (int) duration.toDays(); // 相隔天数
+        return days+1;//包含开头结尾
+    }
+    public static LocalDate convertToLocalDate(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
     /**     目前没用上
